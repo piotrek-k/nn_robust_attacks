178bf7c519f10174d3ff46ea8e2f61aef1f08ff2
Piotr Kozerski <piotrek-k@users.noreply.github.com>
2022-01-12 10:17:16 +0100

    Fix SGD import

--- train_models.py
+++ train_models.py
@@ -10,7 +10,7 @@ import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Conv2D, MaxPooling2D
-from keras.optimizers import SGD
+from tensorflow.keras.optimizers import SGD
 
 import tensorflow as tf
 from setup_mnist import MNIST
